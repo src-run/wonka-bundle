abc885469e14c3f7a0f4a4b43554fffeefa3e95a
Rob Frawley 2nd <rmf@src.run>
2015-12-15 00:41:10 -0500

    removed constructor from exceptions that now have it inherited

--- src/Component/DependencyInjection/Exception/InvalidContainerParameterException.php
+++ src/Component/DependencyInjection/Exception/InvalidContainerParameterException.php
@@ -19,25 +19,6 @@ use Scribe\Wonka\Exception\LogicException;
 class InvalidContainerParameterException extends LogicException implements ContainerExceptionInterface
 {
     /**
-     * @param string|null $message        An error message string (optionally fed to sprintf if optional args are given)
-     * @param int|null    $code           The error code (which should be from ORMExceptionInterface). If null, the value
-     *                                    of ExceptionInterface::CODE_GENERIC will be used.
-     * @param mixed       $previous       The previous exception, if applicable.
-     * @param mixed       $replaceSet,... All extra parameters passed are used to provide replacement values against the
-     *                                    exception message.
-     */
-    public function __construct($message = null, $code = null, $previous = null, ...$replaceSet)
-    {
-        parent::__construct(
-            $this->getFinalMessage((string) $message, ...$replaceSet),
-            $this->getFinalCode((int) $code),
-            $this->getFinalPreviousException($previous)
-        );
-
-        $this->setAttributes([]);
-    }
-
-    /**
      * @return string
      */
     public function getDefaultMessage()
